cc6e6a9a5fab18b4f7211083eff30627f049cbda
thomas-weller <info@thomas-weller.de>
2015-01-03 16:47:15 +0100

    Added test project (using xUnit.Net)
    Added Pbkdf2Tests class

--- PasswordHash.Test/Properties/AssemblyInfo.cs
+++ PasswordHash.Test/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PasswordHash")]
+[assembly: AssemblyTitle("PasswordHash.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PasswordHash")]
+[assembly: AssemblyProduct("PasswordHash.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0d703630-1c78-47ce-aa69-d73bf7465eed")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -28,6 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("PasswordHash.Test")]
-
